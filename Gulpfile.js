cfa4e21639d2666d6f433979d77375188eb94731
Dakuan <dom.barker808@gmail.com>
2015-03-24 11:22:00 +0000

    first

---
+++ Gulpfile.js
@@ -0,0 +1,19 @@
+var less = require('gulp-less'),
+	gulp = require('gulp'),
+    path = require('path');
+
+var paths = {
+    styles: 'less/*.less'
+}
+
+gulp.task('default', ['less'], function(){
+	gulp.watch([paths.styles], ['less']);
+});
+
+gulp.task('less', function() {
+    return gulp.src(paths.styles)
+        .pipe(less({
+            paths: [path.join(__dirname, 'less', 'includes')]
+        }))
+        .pipe(gulp.dest('./build/'));
+});
---
+++ server/index.js
@@ -0,0 +1,17 @@
+var express = require('express'),
+	path = require('path'),
+    app = express();
+
+app.set('views', 'server/views/');
+app.set('view engine', 'jade');
+
+app.use('/less', express.static(path.join(__dirname, '../build')));
+
+app.get('/', function(req, res, next) {
+	res.render('index');
+});
+
+// 6061 is an aluminum alloy. It's what you make aircraft out of.
+app.listen(process.env.PORT || 6061, function() {
+  console.log('listening on *:6061');
+});
